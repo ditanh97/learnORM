c8a9b18c01d1e961a45551ea3eaf7bcff12d18c8
ditutta <51126784+ditutta@users.noreply.github.com>
2019-12-05 01:21:35 +0700

    learn how http works

--- index.js
+++ index.js
@@ -15,9 +15,62 @@ db.authenticate()
 const app = express();
 
 //Body Parser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false})) //to make form data request valid
 
-app.get('/',  (req, res) => res.send('INDEX'))
+// app.get('/',  (req, res) => res.send(req.rawHeaders))
+// app.get('/',  (req, res) => res.send(req.header("user-agent")))
+
+//STATIC FOLDER HANDLING 
+app.use(express.static('public'));
+
+//check 2
+app.post('/contact', (req, res) => {
+    res.send(req.body.name); 
+    // res.send(req.header('Content-Type')); 
+    if (!req.body.name) {
+        return res.status(400).send('Name is required');
+    }
+
+    //DATABASE STUFF
+//    return res.status(201).send(`Thank you ${req.body.name}`)
+
+    //req.body hanya akan bekerja jika app.use(express.json()) dideclare
+})
+
+//AUTHORIZATION SIMULATION
+app.post('/login', (req, res) => {
+    if (!req.header('x-auth-token')) {
+        return res.status(400).send('No token');
+    }
+    if (req.header('x-auth-token') !== '123456') {
+        return res.status(401).send('Not authorized')
+    }
+    res.send('Logged in');
+})
+
+//PUT REQUEST SIMULATION
+app.put('/post/:id', (req,res) => {
+    //database stuff to update the post
+
+    res.json({
+        id: req.params.id,
+        title: req.body.title
+    })
+} )
+
+//DELETE REQUEST SIMULATION
+app.delete('/delete/:id', (req,res) => {
+    //database stuff to update the post
+
+    res.json({
+        msg: `Post ${req.params.id} deleted` 
+    });
+} )
+
+
+
+
 
 //GIG routes -- above the port
 app.use('/', router);
